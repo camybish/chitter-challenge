0092e4634ce262e449a6a4804c54059d0002ef70
Cameron <cameronjamesbishop@googlemail.com>
2023-05-15 17:13:27 +0100

    SQL fixed

--- spec/seeds/seeds_both.sql
+++ spec/seeds/seeds_both.sql
@@ -1,12 +1,8 @@
-DROP TABLE IF EXISTS users;
-
-TRUNCATE TABLE users RESTART IDENTITY;
+TRUNCATE TABLE users RESTART IDENTITY CASCADE;
 
 INSERT INTO users (username, name, email, password) VALUES('Admin', 'Ed Minn', 'EdMinn@email.com', 'password123');
 INSERT INTO users (username, name, email, password) VALUES('camybish', 'Cameron Bishop', 'camybish@piesize.co.uk', 'password234');
 
-
-DROP TABLE IF EXISTS peeps;
 
 TRUNCATE TABLE peeps RESTART IDENTITY;
 
